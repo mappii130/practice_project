8eaa98d230789a6ed15dc57d9922c38e1492e886
mappii130 <90373590+mappii130@users.noreply.github.com>
2024-12-27 00:16:21 +0900

    2nd commit

--- src/main/java/practice/Sample.java
+++ src/main/java/practice/Sample.java
@@ -1,10 +1,35 @@
 package practice;
 
-public class Sample {
+//public class Sample {
+//
+//	public static void main(String[] args) {
+//		System.out.println("Sampleです");
+//		System.out.println("Java");
+//	}
+//
+//}
 
+import java.util.Date;
+
+import Utility.DateFormat;
+
+public class Sample {
+	
 	public static void main(String[] args) {
-		System.out.println("Sampleです");
-		System.out.println("Java");
+		
+		Date date = new Date();
+		
+		String strDate = "";
+		
+		// DateFormatのdateFormatSlashメソッドを使う(yyyy/MM/dd)
+		strDate = DateFormat.dateFormatSlash(date);
+		
+		System.out.println(strDate);
+		
+		// yyyy年MM月dd日
+		strDate = DateFormat.dateFormatKanji(date);
+		
+		System.out.println(strDate);
 	}
 
 }
